e5fefe38028e2977c474e10bf5a9dd1b959dd9d8
Andrey Kolyshkin <a.kolyshkin@corp.vk.com>
2021-08-08 17:02:30 +0300

    Add simple save tournamet edits

--- cmd/import-server/main.go
+++ cmd/import-server/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+    "encoding/json"
+    "io/ioutil"
     "log"
     "net/http"
 
     "github.com/gin-contrib/cors"
     "github.com/gin-gonic/gin"
+    "github.com/google/uuid"
     "github.com/rtsf-ratings/parser"
     "github.com/rtsf-ratings/ratings/db"
     "gorm.io/driver/mysql"
@@ -20,7 +23,6 @@ type Player struct {
 }
 
 type ImportData struct {
-    Players    map[int]*db.Player
     PlayersMap map[int]Player
     Result     parser.Result
 }
@@ -39,7 +41,31 @@ func main() {
         c.IndentedJSON(http.StatusOK, ratings.Players)
     })
 
-    router.POST("/players", func(c *gin.Context) {
+    router.GET("/tournament/:id", func(c *gin.Context) {
+        var data ImportData
+
+        id, err := uuid.Parse(c.Param("name"))
+        if err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": "invalid uuid"})
+            return
+        }
+
+        content, err := ioutil.ReadFile("uploads/" + id.String())
+        if err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": "unknown uuid"})
+            return
+        }
+
+        err = json.Unmarshal([]byte(content), &data)
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "internal error"})
+            return
+        }
+
+        c.IndentedJSON(http.StatusOK, &data)
+    })
+
+    router.POST("/tournament", func(c *gin.Context) {
         var data ImportData
 
         if err := c.BindJSON(&data); err != nil {
@@ -47,7 +73,50 @@ func main() {
             return
         }
 
-        c.IndentedJSON(http.StatusOK, gin.H{"status": "success"})
+        json, err := json.MarshalIndent(data, "", "  ")
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "internal error"})
+            return
+        }
+
+        id := uuid.New()
+
+        err = ioutil.WriteFile("uploads/"+id.String(), json, 0644)
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "internal error"})
+            return
+        }
+
+        c.IndentedJSON(http.StatusOK, gin.H{"status": "success", "id": id.String()})
+    })
+
+    router.POST("/tournament/:id", func(c *gin.Context) {
+        var data ImportData
+
+        if err := c.BindJSON(&data); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": "unknown file type"})
+            return
+        }
+
+        json, err := json.MarshalIndent(data, "", "  ")
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "internal error"})
+            return
+        }
+
+        id, err := uuid.Parse(c.Param("name"))
+        if err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": "invalid uuid"})
+            return
+        }
+
+        err = ioutil.WriteFile("uploads/"+id.String(), json, 0644)
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "internal error"})
+            return
+        }
+
+        c.IndentedJSON(http.StatusOK, gin.H{"status": "success", "id": id.String()})
     })
 
     router.Run("127.0.0.1:8083")
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.3
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/rtsf-ratings/parser v0.0.0-20210807154804-e718ed02f72d // indirect
 	gorm.io/driver/mysql v1.1.1
 	gorm.io/gorm v1.21.12
